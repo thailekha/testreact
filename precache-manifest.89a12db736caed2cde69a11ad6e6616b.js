545cb1570ff48efab36f82be3474e06913bac754
Thai Le <thailekha@yahoo.com>
2019-03-25 20:13:38 +0000

    Updates

---
+++ precache-manifest.89a12db736caed2cde69a11ad6e6616b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1d0bc4833f5a10c60360",
+    "url": "/testreact/static/js/runtime~main.30e0e851.js"
+  },
+  {
+    "revision": "83f20a6e85465fdb7a11",
+    "url": "/testreact/static/js/main.d6a799cc.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/testreact/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "83f20a6e85465fdb7a11",
+    "url": "/testreact/static/css/main.d6a286a3.chunk.css"
+  },
+  {
+    "revision": "f6553b02c4b6086c3e9b8e8b29895bda",
+    "url": "/testreact/index.html"
+  }
+];
